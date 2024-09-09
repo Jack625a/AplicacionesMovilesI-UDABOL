f8a4536ce799c40109cb09fc0e6c13fae2bbaa08
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-09-09 16:23:09 -0400

    TextField

--- app1.py
+++ app1.py
@@ -16,7 +16,8 @@ def main(page):
     imagen=ft.Image(
         src="https://habrastorage.org/webt/4m/3k/qh/4m3kqhpn2-xb59x_v58sxmyioki.png",
         width=300,
-        height=200        
+        height=200,
+        border_radius=20      
         )
 
     #Crear el boton para mostrar la funcion 
@@ -24,10 +25,14 @@ def main(page):
         text="Mostrar Mensaje",
         on_click=mostrarAlerta
     )
+
+    #Entradad de datos
+    nombre=ft.TextField(label="Ingrese su nombre")
+    celular=ft.TextField(label="Ingrese su celular")
     
 
     #Agregar el componente a la aplicacion
-    page.add(imagen,boton)
+    page.add(imagen,nombre,celular,boton)
 
 ft.app(target=main)
 
